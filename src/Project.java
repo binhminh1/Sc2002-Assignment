c1045d74faf6810027d3c225b3906388d713c0f8
NtuDarthVader <126957111+NtuDarthVader@users.noreply.github.com>
2023-03-13 23:39:18 +0800

    Project and Student class

--- src/Project.java
+++ src/Project.java
@@ -3,4 +3,50 @@ public class Project {
     private String SupervisorName;
     private String SupervisorEmail;
     private String ProjectTitle;
+
+    private String StudentName;
+
+    private String StudentEmailAddress;
+
+
+    private ProjectStatus status;
+    public String getProjectId(){
+        return ProjectId;
+    }
+    public ProjectStatus getStatus(){
+        return status;
+    }
+    public String getSupervisorName(){
+        return SupervisorName;
+    }
+
+    public String getSupervisorEmail(){
+        return SupervisorEmail;
+    }
+
+    public String getProjectTitle(){
+        return ProjectTitle;
+    }
+
+    public void setProjectId(String projectId){
+        ProjectId = projectId;
+    }
+
+    public void setSupervisorName(String supervisorName){
+        SupervisorName = supervisorName;
+    }
+
+    public void setSupervisorEmail(String supervisorEmail){
+        SupervisorEmail = supervisorEmail;
+    }
+
+    public void setProjectTitle(String projectTitle){
+        ProjectTitle = projectTitle;
+    }
+
+    public void setStatus(ProjectStatus status){
+        this.status = status;
+    }
+
+
 }
--- src/Student.java
+++ src/Student.java
@@ -2,10 +2,47 @@ import java.util.Objects;
 
 public class Student implements User{
     private String UserId;
-    private String Password;
+    private String Password = "password";
     private String Email;
     private String Name;
-    private String Project;
+
+
+    public String getUserId(){
+        return UserId;
+    }
+
+    public String getPassword(){
+        return Password;
+    }
+
+    public String getEmail(){
+        return Email;
+    }
+
+    public String getName(){
+        return Name;
+    }
+
+    public String getProject(){
+        return Name;
+    }
+
+    public String getUserId(){
+        return UserId;
+    }
+
+    public void setPassword(String password){
+        this.Password = password;
+    }
+
+    public void setEmail(String email){
+        this.Email = email;
+    }
+
+    public String setName(String name){
+        this.Name = name;
+    }
+
 
     @Override
     public void changePassword(String password){
@@ -13,7 +50,6 @@ public class Student implements User{
     }
     public boolean login(String userid, String password){
         return (Objects.equals(UserId, userid) && Objects.equals(Password, password));
-
     }
 
 
