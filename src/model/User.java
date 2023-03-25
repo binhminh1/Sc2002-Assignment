7c9c5a2db6250ea746a0f1ef52e48c741682e6be
NtuDarthVader <126957111+NtuDarthVader@users.noreply.github.com>
2023-03-25 21:01:43 +0800

    Repository and change User

--- src/model/User.java
+++ src/model/User.java
@@ -1,6 +1,8 @@
 package model;
 
 public interface User {
+
     public void changePassword(String password);
     public boolean login(String userid, String password);
+    public String getID();
 }
---
+++ src/repository/ProjectRepository.java
@@ -0,0 +1,5 @@
+package repository;
+
+public class ProjectRepository {
+    ArrayList<Project> ProjectList = new
+}
