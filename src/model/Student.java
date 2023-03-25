db74d81994d250b67127f3905870c568f8b0b0be
shanyi26 <127191556+shanyi26@users.noreply.github.com>
2023-03-25 19:11:38 +0800

    Update Student.java

--- src/model/Student.java
+++ src/model/Student.java
@@ -3,55 +3,62 @@ package model;
 import java.util.Objects;
 
 public class Student implements User{
-    private String UserId;
-    private String Password = "password";
-    private String Email;
-    private String Name;
-
-
-    public String getUserId(){
-        return UserId;
+    private String userId;
+    private String password;
+    private String email;
+    private String name;
+    public Student(String userId, String name, String email) {
+        this.userId = userId;
+        this.name = name;
+        this.email = email;
+        this.password = "password";
     }
 
+
     public String getPassword(){
-        return Password;
+        return password;
     }
 
     public String getEmail(){
-        return Email;
+        return email;
     }
 
     public String getName(){
-        return Name;
+        return name;
     }
 
     public String getProject(){
-        return Name;
+        return name;
     }
 
     public String getUserId(){
-        return UserId;
+        return userId;
     }
 
     public void setPassword(String password){
-        this.Password = password;
+        this.password = password;
     }
 
     public void setEmail(String email){
-        this.Email = email;
+        this.email = email;
     }
 
-    public String setName(String name){
-        this.Name = name;
+    public void setName(String name){
+        this.name = name;
     }
 
 
     @Override
+    public String getID() {
+        return getUserId();
+    }
+
+    @Override
     public void changePassword(String password){
-        Password = password;
+        this.password = password;
     }
     public boolean login(String userid, String password){
-        return (Objects.equals(UserId, userid) && Objects.equals(Password, password));
+        return (Objects.equals(userId, userid) && Objects.equals(this.password, password));
     }
 
 
