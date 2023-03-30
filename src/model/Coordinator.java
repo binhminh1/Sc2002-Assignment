55af6488b5fd09b475f147599f9bd156a9712898
Tran Khanh Linh (Evelyn) <119790948+khanhlin003@users.noreply.github.com>
2023-03-30 22:16:11 +0800

    Update Coordinator.java

--- src/model/Coordinator.java
+++ src/model/Coordinator.java
@@ -16,41 +16,9 @@ public class Coordinator extends User{
     }
 
 
-    public String getPassword(){
-        return password;
-    }
+    
+   
 
-    public String getEmail(){
-        return email;
-    }
-
-    public String getName(){
-        return name;
-    }
-
-    public String getProject(){
-        return name;
-    }
-
-    public String getUserId(){
-        return userId;
-    }
-
-    public void setPassword(String password){
-        this.password = password;
-    }
-
-    public void setEmail(String email){
-        this.email = email;
-    }
-
-    public void setName(String name){
-        this.name = name;
-    }
-
-    public String getID() {
-        return getUserId();
-    }
 
 
 }
