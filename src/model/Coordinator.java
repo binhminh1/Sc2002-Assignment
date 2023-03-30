89f87c3573d471da21ce9302c09b711dc6141dde
Tran Khanh Linh (Evelyn) <khanhlin003@e.ntu.edu.sg>
2023-03-30 22:02:09 +0800

    Update Coordinator.java

--- src/model/Coordinator.java
+++ src/model/Coordinator.java
@@ -14,18 +14,82 @@ public class Coordinator extends User{
         this.email = email;
         this.password = "password";
     }
-
-    public String getProject(){
-        return name;
+    
+    public void allocateProject(Project project, Student student) {
+        if (project.getStatus() == ProjectStatus.AVAILABLE) {
+            project.setStudent(student);
+            project.setStatus(ProjectStatus.ALLOCATED);
+            student.setProject(project);
+        } else {
+            throw new IllegalArgumentException("Project is not available for allocation");
+        }
     }
 
-    @Override
-    public void changePassword(String password){
-        this.password = password;
+    public void changeSupervisor(Project project, Supervisor supervisor) {
+        project.setSupervisor(supervisor);
     }
 
-    public boolean login(String userid, String password){
-        return (Objects.equals(userId, userid) && Objects.equals(this.password, password));
+    public void deregisterStudent(Student student) {
+        Project project = student.getProject();
+        if (project != null) {
+            project.setStudent(null);
+            project.setStatus(ProjectStatus.AVAILABLE);
+            student.setProject(null);
+            student.setDeregistered(true);
+        } else {
+            throw new IllegalArgumentException("Student is not registered for any project");
+        }
     }
+
+    public List<Project> getAvailableProjects() {
+        List<Project> availableProjects = new ArrayList<>();
+        for (Project project : projects) {
+            if (project.getStatus() == ProjectStatus.AVAILABLE) {
+                availableProjects.add(project);
+            }
+        }
+        return availableProjects;
+    }
+
+    public List<Project> getUnavailableProjects() {
+        List<Project> unavailableProjects = new ArrayList<>();
+        for (Project project : projects) {
+            if (project.getStatus() == ProjectStatus.UNAVAILABLE) {
+                unavailableProjects.add(project);
+            }
+        }
+        return unavailableProjects;
+    }
+
+    public List<Project> getReservedProjects() {
+        List<Project> reservedProjects = new ArrayList<>();
+        for (Project project : projects) {
+            if (project.getStatus() == ProjectStatus.RESERVED) {
+                reservedProjects.add(project);
+            }
+        }
+        return reservedProjects;
+    }
+
+    public List<Project> getAllocatedProjects() {
+        List<Project> allocatedProjects = new ArrayList<>();
+        for (Project project : projects) {
+            if (project.getStatus() == ProjectStatus.ALLOCATED) {
+                allocatedProjects.add(project);
+            }
+        }
+        return allocatedProjects;
+    }
+
+    public List<Project> searchProjectsByStatus(ProjectStatus status) {
+        List<Project> projectsByStatus = new ArrayList<>();
+        for (Project project : projects) {
+            if (project.getStatus() == status) {
+                projectsByStatus.add(project);
+            }
+        }
+        return projectsByStatus;
+    }
+
 
 }
