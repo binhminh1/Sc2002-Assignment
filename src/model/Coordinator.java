c9d62821924b9924f0460623b9bed9a405c538d3
Tran Khanh Linh (Evelyn) <khanhlin003@e.ntu.edu.sg>
2023-03-30 21:58:07 +0800

    Update Coordinator.java

--- src/model/Coordinator.java
+++ src/model/Coordinator.java
@@ -15,48 +15,15 @@ public class Coordinator extends User{
         this.password = "password";
     }
 
-
-    public String getPassword(){
-        return password;
-    }
-
-    public String getEmail(){
-        return email;
-    }
-
-    public String getName(){
-        return name;
-    }
-
     public String getProject(){
         return name;
-    }
-
-    public String getUserId(){
-        return userId;
-    }
-
-    public void setPassword(String password){
-        this.password = password;
-    }
-
-    public void setEmail(String email){
-        this.email = email;
-    }
-
-    public void setName(String name){
-        this.name = name;
-    }
-
-
-    public String getID() {
-        return getUserId();
     }
 
     @Override
     public void changePassword(String password){
         this.password = password;
     }
+
     public boolean login(String userid, String password){
         return (Objects.equals(userId, userid) && Objects.equals(this.password, password));
     }
