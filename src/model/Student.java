048b2d329129e2775cbae02f5047b5149a20a796
shuangshuu <tayshushuang@gmail.com>
2023-04-16 14:15:58 +0800

    1

--- src/model/Student.java
+++ src/model/Student.java
@@ -32,6 +32,9 @@ public class Student extends User implements ViewRequestHistory {
         this.status = StudentStatus.UNREGISTERED;
     }
 
+    /**
+     * Makes use of overriding for student to login
+     */
     @Override
     public Boolean login() {
         System.out.println("Enter your password: ");
@@ -63,8 +66,8 @@ public class Student extends User implements ViewRequestHistory {
     }
 
     /**
-     * changes the student status (PENDING, DEREGISTERED, REGISTERED)
-     * @param status
+     * changes the student status
+     * @param status (PENDING, DEREGISTERED, REGISTERED)
      */
     public void changeStatus(StudentStatus status) {
         this.status = status;
@@ -116,7 +119,7 @@ public class Student extends User implements ViewRequestHistory {
     /**
      * Allows student to send a request to the coordinator to select a project
      * @param student
-     * @return 
+     * @return request
      */
     public Request sendSelectProjectRequest(Student student) {
         if (student.getStatus() == UNREGISTERED) {
@@ -154,7 +157,7 @@ public class Student extends User implements ViewRequestHistory {
     /**
      * Allows student to send a request to the coordinator to deregister from allocated project
      * @param student
-     * @return
+     * @return request
      */
     public Request sendDeregisterProjectRequest(Student student) {
         if (student.getStatus() == StudentStatus.REGISTERED) {
@@ -170,6 +173,7 @@ public class Student extends User implements ViewRequestHistory {
 
     /**
      * Allows student to view all the requests that he sent out
+     * @return request history
      */
     public List<String> viewOutgoingRequestsHistory() {
         List<String> requestHistory = new ArrayList<>();
@@ -240,7 +244,7 @@ public class Student extends User implements ViewRequestHistory {
     /**
      * Allows student to send a request to supervisor to change the project title
      * @param student
-     * @return
+     * @return request
      */
     public Request sendChangeTitleRequest(Student student){
         if (student.getStatus() == StudentStatus.REGISTERED) {
